3f266b80fd19deb2d1e4cf9e65e929fd840490d6
Damian de Vega <damiandevega@gmail.com>
2020-11-18 11:47:24 -0500

    adding async demo tests

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+it("async test 1", (done) => {
+  setTimeout(done, 100);
+});
+
+it("async test 2", () => {
+  return new Promise((resolve) => setTimeout(resolve, 1500));
+});
+
+it("async test 3", async () => await setTimeout(() => {}, 1000));
--- src/components/QuestionList.test.js
+++ src/components/QuestionList.test.js
@@ -1,5 +1,25 @@
 describe(`Question List`, () => {
+  beforeEach(() => {
+    console.log("Before each");
+  });
+
+  beforeAll(() => {
+    console.log("Before all");
+  });
+
+  afterEach(() => {
+    console.log("After each");
+  });
+
+  afterAll(() => {
+    console.log("After all");
+  });
+
   it(`should display a list of items`, () => {
     expect(2 + 2).toEqual(4);
+  });
+
+  it.skip(`should display a list of items`, () => {
+    expect(2 + 2).toEqual(5);
   });
 });
